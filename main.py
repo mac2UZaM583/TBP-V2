dbc99af510a450a6fb0703638e0f6eeca58f2d72
mac2UZaM583 <163586801+mac2UZaM583@users.noreply.github.com>
2024-06-19 17:08:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -12,38 +12,33 @@ sl = 0.030
 
 def main():
     global headers, tp, sl, url_count
-    # Инициализация контроля программы засчет цикла
     while True:
         try:
-            while True:
-                try:
-                    url = f'https://t.me/pump_dump_screener_demo/{url_count}'
-                    content = prsrpp(url, headers)
-                    timeNow = int(time.time())
-                    url_count += 1
-                            
-                    ticker = get_ticker(content)
-                    balance_usdt = get_balance()
-                    balanceWL = float(balance_usdt)
-                    mark_price = get_last_price(ticker)
-                    roundQty =  get_roundQty(ticker)
-                    if str(content).count('🔴') == 1:
-                        if balance_usdt != 0:
-                            side = klineValidation(ticker, 'Buy', mark_price, roundQty, timeNow)
-                            place_order(ticker, side, mark_price, roundQty, balanceWL, tp, sl)
-                    if str(content).count('🟢') == 1:
-                        if balance_usdt != 0:
-                            side = klineValidation(ticker, 'Sell', mark_price, roundQty, timeNow)
-                            place_order(ticker, side, mark_price, roundQty, balanceWL, tp, sl)
+            url = f'https://t.me/pump_dump_screener_demo/{url_count}'
+            content = prsrpp(url, headers)
+            timeNow = int(time.time())
+            url_count += 1
+                    
+            ticker = get_ticker(content)
+            balance_usdt = get_balance()
+            balanceWL = float(balance_usdt)
+            mark_price = get_last_price(ticker)
+            roundQty =  get_roundQty(ticker)
+            if str(content).count('🔴') == 1:
+                if balance_usdt != 0:
+                    side = klineValidation(ticker, 'Buy', mark_price, roundQty, timeNow)
+                    place_order(ticker, side, mark_price, roundQty, balanceWL, tp, sl)
+            if str(content).count('🟢') == 1:
+                if balance_usdt != 0:
+                    side = klineValidation(ticker, 'Sell', mark_price, roundQty, timeNow)
+                    place_order(ticker, side, mark_price, roundQty, balanceWL, tp, sl)
 
-                    # Сохранение значений в текстовой файл
-                    with open('urlCount.txt', 'w', encoding='utf-8') as f:
-                        f.write(str(url_count))
-                    with open('urlCount.txt', 'r', encoding='utf-8') as f:
-                        url_count = int(f.read())
-                    break
-                except Exception as er:
-                    pprint(er)
+            # Сохранение значений в текстовой файл
+            with open('urlCount.txt', 'w', encoding='utf-8') as f:
+                f.write(str(url_count))
+            with open('urlCount.txt', 'r', encoding='utf-8') as f:
+                url_count = int(f.read())
+            break
         except Exception as er:
             pprint(er)
 
