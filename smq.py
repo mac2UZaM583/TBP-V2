c8b5845f32f21836c14d2fcfb0ec7cd2ed42d939
mac2UZaM583 <163586801+mac2UZaM583@users.noreply.github.com>
2024-06-22 21:00:53 +0300

    Add files via upload

--- smq.py
+++ smq.py
@@ -2,52 +2,65 @@ from pybit.unified_trading import HTTP
 import time
 from decimal import Decimal
 from datetime import datetime
-from multiprocessing import Process, Queue
 
 THRESHOLD_PERCENT = 3
 session = HTTP()
 
-def fetch_data(dataQueue):
-    while True:
-        data = session.get_tickers(category='linear')['result']['list']
-        print('получил новые данные')        
-        dataQueue.put(data)
-        time.sleep(60)
+def fetch_data():
+    data = session.get_tickers(category='linear')['result']['list']
+    print(f'{datetime.now()}: Получил новые данные')
+    return data
 
-def process_data(dataQueue, result_queue):
-    while True:
-        data_old = dataQueue.get()
-        prices_old = {price['symbol']: Decimal(price['lastPrice']) for price in data_old}
-        while True:
-            time.sleep(1)
-            print(f'Check data. Time: {datetime.now()}')
-            
-            data_new = session.get_tickers(category='linear')['result']['list']
-            for price_new in data_new:
-                symbol = price_new['symbol']
-                if symbol in prices_old:
-                    percent_change = round(((Decimal(price_new['lastPrice']) - prices_old[symbol]) / prices_old[symbol]) * 100, 2)
-                    if abs(percent_change) >= THRESHOLD_PERCENT and 'USDT' in symbol:
-                        result_queue.put((symbol, percent_change))
-            
 def smq():
-    dataQueue = Queue()
-    result_queue = Queue()
-    process1 = Process(target=fetch_data, args=(dataQueue,))
-    process2 = Process(target=process_data, args=(dataQueue, result_queue))
-    process1.start()
-    process2.start()
+    data_old = fetch_data()
+    prices_old = {price['symbol']: Decimal(price['lastPrice']) for price in data_old}
+    start_time = time.time()
+
     while True:
-        result = result_queue.get()
-        if result is not None:
-            with open('/CODE_PROJECTS/SMQ-N & Python/signal.txt', 'w', encoding='utf-8') as f:
-                if result[1] < 0:
-                    f.write(f'🔴Ticker: {result[0]}\n'
-                            f'Percent - {result[1]}%')
-                if result[1] > 0:
-                    f.write(f'🟢Ticker: {result[0]}\n'
-                            f'Percent - {result[1]}%')
-            return result
+        # Проверка, прошла ли минута для обновления старых данных
+        if time.time() - start_time >= 60:
+            data_old = fetch_data()
+            prices_old = {price['symbol']: Decimal(price['lastPrice']) for price in data_old}
+            start_time = time.time()
+
+        time.sleep(0.5)
+        print(f'Check data. Time: {datetime.now()}')
+        
+        data_new = session.get_tickers(category='linear')['result']['list']
+        for price_new in data_new:
+            symbol = price_new['symbol']
+            if symbol in prices_old:
+                percent_change = round(((Decimal(price_new['lastPrice']) - prices_old[symbol]) / prices_old[symbol]) * 100, 2)
+                if abs(percent_change) >= THRESHOLD_PERCENT and 'USDT' in symbol:
+                    with open('/CODE_PROJECTS/SMQ-N & Python/signal.txt', 'w', encoding='utf-8') as f:
+                        if percent_change < 0:
+                            f.write(f'🔴Ticker: {symbol}\n'
+                                    f'Percent: {percent_change}%')
+                        if percent_change > 0:
+                            f.write(f'🟢Ticker: {symbol}\n'
+                                    f'Percent: {percent_change}%')
+                    return symbol, percent_change
+            
+
+
+# def smq():
+#     dataQueue = Queue()
+#     result_queue = Queue()
+#     process1 = Process(target=fetch_data, args=(dataQueue,))
+#     process2 = Process(target=process_data, args=(dataQueue, result_queue))
+#     process1.start()
+#     process2.start()
+#     while True:
+#         result = result_queue.get()
+#         if result is not None:
+            # with open('/CODE_PROJECTS/SMQ-N & Python/signal.txt', 'w', encoding='utf-8') as f:
+            #     if result[1] < 0:
+            #         f.write(f'🔴Ticker: {result[0]}\n'
+            #                 f'Percent - {result[1]}%')
+            #     if result[1] > 0:
+            #         f.write(f'🟢Ticker: {result[0]}\n'
+            #                 f'Percent - {result[1]}%')
+#             return result
 
 # if __name__ == '__main__':
 #     while True:
