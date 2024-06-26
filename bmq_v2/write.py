5c5ca512856e9e3ff81fdfe599752bbc5810ef7b
mac2UZaM583 <163586801+mac2UZaM583@users.noreply.github.com>
2024-06-27 01:05:03 +0300

    Add files via upload

--- bmq_v2/write.py
+++ bmq_v2/write.py
@@ -6,9 +6,20 @@ from bmq_v2.keys import session
 from bmq_v2.read import get_roundQty, get_avg_position_price
 from datetime import datetime
 from decimal import Decimal as D
+import traceback
 
 '''ORDERS ↓
 '''
+def cancel_position():
+    position = session.get_positions(category='linear', settleCoin='USDT')['result']['list'][-1]
+    side = 'Buy' if position['side'] == 'Sell' else 'Sell'
+    session.place_order(category='linear',
+                        symbol=position['symbol'],
+                        side=side,
+                        orderType='Market',
+                        qty=position['size'],
+                        reduceOnly=True)
+
 def orders_clear(orders_tp):
     if not orders_tp:
         session.cancel_all_orders(category="linear", settleCoin='USDT')
@@ -23,8 +34,9 @@ def place_order_limit(symbol, side, qty, price, i=None):
             isLeverage=10,
             tpTriggerBy='LastPrice', slTriggerBy='LastPrice'
         )
-    except Exception as er:
-        print(er)
+    except:
+        cancel_position()
+        er = traceback.format_exc()
         with open('/CODE_PROJECTS/SMQ-N & Python/signal.txt', 'w', encoding='utf-8') as f:
             f.write(f'Ошибка в выставлении {i} лимитного ордера: \n{er}\n Время: {datetime.now()}')
 
@@ -65,7 +77,10 @@ def TP(position, orders_tp, orders_limit_num, tp):
         avg_price = D(position['avgPrice'])
         round_qty = get_roundQty(symbol)
         tp_price = round(avg_price + ((avg_price * tp[-(orders_limit_num + 1)]) * (-1 if side == 'Sell' else 1)), round_qty[0])
-        session.set_trading_stop(category='linear', symbol=symbol, tpslMode='Full', takeProfit=tp_price, positionIdx=0)
+        try:
+            session.set_trading_stop(category='linear', symbol=symbol, tpslMode='Full', takeProfit=tp_price, positionIdx=0)
+        except:
+            cancel_position()
         
 
 def SL(position, orders_sl, orders_limit, sl):
@@ -75,7 +90,10 @@ def SL(position, orders_sl, orders_limit, sl):
         avg_price = get_avg_position_price()
         round_qty = get_roundQty(symbol)
         sl_price = round(avg_price + ((avg_price * sl * (1 if side == 'Sell' else -1))), round_qty[0])
-        session.set_trading_stop(category='linear', symbol=symbol, tpslMode='Full', stopLoss=sl_price, positionIdx=0)
+        try:
+            session.set_trading_stop(category='linear', symbol=symbol, tpslMode='Full', stopLoss=sl_price, positionIdx=0)
+        except:
+            cancel_position()
 
 '''MORE ↓
 '''
